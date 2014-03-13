6da9767f6513aee72a2369a6d3a513fb1d0a6bfb
reesef <reesef@gmail.com>
2014-03-13 00:05:52 -0700

    Work through ex20

---
+++ ex20.py
@@ -0,0 +1,44 @@
+# import argv for command line args
+from sys import argv
+
+# set these args to script and input file
+script, input_file = argv
+
+# pass this function a file (f) and it will print the whole thing
+def print_all(f):
+	print f.read()
+
+# find the first line in the file
+def rewind(f):
+	f.seek(0)
+
+# print line number and then read the line
+# this function takes 2 args, line_count (a number) and a file
+def print_a_line(line_count, f):
+	print line_count, f.readline()
+
+# set the current file to the second command line arg
+current_file = open(input_file)
+
+# print the entirety of the current file
+print "First lets print the entire file:\n"
+print_all(current_file)
+
+# find the first line in the file
+print "Now, lets rewind like a tape."
+rewind(current_file)
+
+
+print "Now lets print three lines:"
+# set the current line to line 1
+current_line = 1
+# print the current_line and the first line from that file
+print_a_line(current_line, current_file)
+
+# increment current line, then print it and the line
+current_line += 1
+print_a_line(current_line, current_file)
+
+# increment to the next line, then print it out
+current_line += 1
+print_a_line(current_line, current_file)
